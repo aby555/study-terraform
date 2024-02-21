acdcd5f40041c328a13bc7557b2398bda070f6b5
abyal <alex.aby555@gmail.com>
2024-02-21 13:42:25 +0000

    latest code

--- ec2-creation.tf
+++ ec2-creation.tf
@@ -18,6 +18,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAQ6DNTPZ4V2WB3K35"
-  secret_key = "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"
+  access_key = "AKIAYUMQ5Q2SYYKIWLCQ"
+  secret_key = "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"
 }
