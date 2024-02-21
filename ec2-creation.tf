9ea86e8be8bec41d4b12b92f165b07f0983d825a
abyal <alex.aby555@gmail.com>
2024-02-21 14:43:37 +0000

    latest code

--- ec2-creation.tf
+++ ec2-creation.tf
@@ -21,3 +21,19 @@ provider "aws" {
   access_key = ""
   secret_key = ""
 }
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
