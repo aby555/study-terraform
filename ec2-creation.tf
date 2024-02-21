fef0bd1020afd4618660ae6b40ce0ef4af194c50
abyal <alex.aby555@gmail.com>
2024-02-21 12:16:33 +0000

    latest code

--- ec2-creation.tf
+++ ec2-creation.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2-instance" {
 
   ami = "ami-0f3c7d07486cad139"
-  instance_type = "t2-micro"
+  instance_type = "t2.micro"
   count = 2
   key_name = "app-server"
   tags = {
